78941fa975b704559095a70af9d7d1f96c642698
aneeshsanils <nnm22cs027@nmamit.in>
2023-08-10 15:11:18 +0530

    FEWD LAB EXP

---
+++ first.R
@@ -0,0 +1,51 @@
+v <- T
+print(class(v))
+v<-23.5
+print(class(v))
+x <- 35
+print(class(x))
+y<-100
+print(x)
+print(y)
+z=250
+total=x+y+z
+print(total)
+a=round(987658.646616, digits=-3)
+print(a)
+
+b<-2L
+print(class(b))
+
+c<-2+5i
+print(class(c))
+
+d<-"TRUE"
+print(class(d))
+
+e<-charToRaw("Hello")
+print(class(e))
+print(e)
+
+f<-c(5,8,65,29,100)
+print(class(f))
+
+apple<-c('red','green','yellow')
+print(apple)
+print(class(apple))
+
+logical<-TRUE
+print(logical)
+print(class(logical))
+
+vector<-c(45,67,98,85,46,52,98)
+print(vector[2])
+print(vector[-2])
+print(vector[2:4])
+print(vector[-2:-4])
+print(vector[c(2,4)])
+
+print(length(vector))
+
+g<-c(.3,6,5,9,65,26,5.5,794,89.64,9.8476,9.46,4,10,"R program","Lab",TRUE,F)
+print(class(g))
+print(g)
